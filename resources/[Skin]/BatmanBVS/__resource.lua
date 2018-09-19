bbae1da34ec430ac7a3a51ef8cf5f09c6a5703cf
Maxime MAURER <maxime.maurer@viacesi.fr>
2018-09-19 11:23:03 +0200

    add some skins to the server

---
+++ resources/[Skin]/BatmanBVS/__resource.lua
@@ -0,0 +1,17 @@
+-- Manifest
+resource_manifest_version 'f15e72ec-3972-4fe4-9c7d-afc5394ae207'
+
+files {
+	'data/BatmanBVS.meta',
+
+	}
+	
+
+
+data_file 'PED_METADATA_FILE' 'data/BatmanBVS.meta'
+
+
+
+
+
+--client_script { 'client/vehicle_name.lua' }
