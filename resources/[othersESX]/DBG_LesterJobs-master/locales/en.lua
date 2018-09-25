64407893d12663ab97e1644ef2cfef07c8cd0a3b
Maxime MAURER <maxime.maurer@viacesi.fr>
2018-09-25 09:30:14 +0200

    rectif connerie epolas

--- resources/[othersESX]/DBG_LesterJobs-master/locales/en.lua
+++
@@ -1,7 +0,0 @@
-Locales['en'] = {
-
-	['new_job'] = '<C>~o~Now you work for me!</C>',
-	['access_job_list'] = 'press ~INPUT_PICKUP~ to access the ~o~Lester job list~s~.',
-	['Lester_Jobs'] = 'Criminal Jobs',
-
-}
