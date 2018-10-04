fd7e97c8f8bbe0fd9ad48ac549778f6466a81c4e
epolas83 <43386730+epolas83@users.noreply.github.com>
2018-10-04 12:05:39 -0700

    MaJ

--- resources/[esx]/esx_policejob/config.lua
+++ resources/[esx]/esx_policejob/config.lua
@@ -15,7 +15,7 @@ Config.EnableLicenses             = true -- enable if you're using esx_license
 Config.EnableHandcuffTimer        = true -- enable handcuff timer? will unrestrain player after the time ends
 Config.HandcuffTimer              = 10 * 60000 -- 10 mins
 
-Config.EnableJobBlip              = false -- enable blips for colleagues, requires esx_society
+Config.EnableJobBlip              = true -- enable blips for colleagues, requires esx_society
 
 Config.MaxInService               = -1
 Config.Locale                     = 'fr'
